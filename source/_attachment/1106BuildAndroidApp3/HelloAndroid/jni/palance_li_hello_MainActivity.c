9a4cefd1c61c7b3e7a3cafe934f4a5b4aaefb511
palance <palanceli@163.com>
2016-11-07 22:26:34 +0800

    commit

--- source/_attachment/1106BuildAndroidApp3/HelloAndroid/jni/palance_li_hello_MainActivity.c
+++
@@ -1,6 +0,0 @@
-#include <string.h>
-#include "palance_li_hello_MainActivity.h"
-JNIEXPORT jstring JNICALL Java_palance_li_hello_MainActivity_stringFromJNI(JNIEnv* env, jobject _this)
-{
-    return (*env)->NewStringUTF(env, "This is from JNI!");
-}
